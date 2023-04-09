6fac212514ffff528c29996ce5d7e36ac8be5933
Akash <akash.dubey@akudo.in>
2023-04-09 20:36:57 +0530

    repo added

--- lib/repository/facts_repo.dart
+++ lib/repository/facts_repo.dart
@@ -1,1 +1,23 @@
+import 'package:only_facts/model/facts_model.dart';
+import 'package:only_facts/network/api_provider.dart';
 
+class FactsRepo {
+  Future<List<Facts>> getFacts() async {
+    List<Facts> factsList = [];
+    var endPoint = "v1/facts?limit=10";
+    try {
+      var data = await ApiProvider().getCall(endPoint);
+      if (data['error'] != null) {
+        throw "error occured";
+      }
+      factsList = (data["result"] as List)
+          .map((facts) => Facts.fromJson(facts))
+          .toList();
+
+      return factsList;
+    } catch (e) {
+      print("Error is $e");
+      throw "Something went wrong";
+    }
+  }
+}
